51914b19c458e7d28ef2abe1e0dc81fc6f32a2a4
Your Name <you@example.com>
2020-09-17 15:17:26 +0200

    Databasr, tag & filter

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -5,8 +5,10 @@ import com.example.repos.MessasgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -24,6 +26,28 @@ public class GreetingController {
         public String main(Map<String, Object> model) {
             Iterable<Message> messages = messasgeRepository.findAll();
 
+            model.put("messages", messages);
+            return "main";
+        }
+
+        @PostMapping
+        public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+            Message message = new Message(text, tag);
+            messasgeRepository.save(message);
+            Iterable<Message> messages = messasgeRepository.findAll();
+            model.put("messages", messages);
+            return "main";
+
+        }
+
+        @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+            Iterable<Message> messages;
+            if (filter != null && !filter.isEmpty()) {
+                messages = messasgeRepository.findByTag(filter);
+            } else {
+                messages = messasgeRepository.findAll();
+            }
             model.put("messages", messages);
             return "main";
         }
