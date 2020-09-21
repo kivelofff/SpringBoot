746f0c84fd3705e4a24fe5c5a5910373912d3a10
Your Name <you@example.com>
2020-09-21 18:18:02 +0200

    one to many!

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.example.controller;
 
 import com.example.domain.Message;
+import com.example.domain.User;
 import com.example.repos.MessasgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,8 +32,8 @@ public class MainController {
         }
 
         @PostMapping("/main")
-        public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-            Message message = new Message(text, tag);
+        public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+            Message message = new Message(text, tag, user);
             messasgeRepository.save(message);
             Iterable<Message> messages = messasgeRepository.findAll();
             model.put("messages", messages);
--- src/main/java/com/example/domain/Message.java
+++ src/main/java/com/example/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -12,13 +9,21 @@ public class Message {
     private Integer id;
     private String text;
     private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User author) {
         this.text = text;
         this.tag = tag;
+        this.author = author;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
     }
 
     public Integer getId() {
@@ -31,5 +36,13 @@ public class Message {
 
     public String getTag() {
         return tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
