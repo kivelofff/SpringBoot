dcb83fee80d1488f55d1e0c17d0962926abea5a6
Your Name <you@example.com>
2020-09-21 16:22:13 +0200

    In te middle of Spring Security2

--- src/main/java/com/example/controller/RegistrationController.java
+++ src/main/java/com/example/controller/RegistrationController.java
@@ -1,12 +1,16 @@
 package com.example.controller;
 
 import com.example.domain.User;
+import com.example.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/registration")
     public String registration() {
         return "registration";
@@ -14,7 +18,7 @@ public class RegistrationController {
 
     @PostMapping
     public String addUser(User user) {
-
+        userRepository.findByUsername(user.getUsername());
         return "redirect:/login";
     }
 }
