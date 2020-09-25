235da4389139a3dd3791bab9b4ffc055cb129d4b
ladmin <kivelofff@gmail.com>
2020-09-25 09:08:19 +0200

    trying freemarker-7root3

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.repos.MessasgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,10 +25,15 @@ public class MainController {
         }
 
         @GetMapping("/main")
-        public String main(Map<String, Object> model) {
+        public String main(@RequestParam(required = false) String filter, Model model) {
             Iterable<Message> messages = messasgeRepository.findAll();
-
-            model.put("messages", messages);
+            if (filter != null && !filter.isEmpty()) {
+                messages = messasgeRepository.findByTag(filter);
+            } else {
+                messages = messasgeRepository.findAll();
+            }
+            model.addAttribute("messages", messages);
+            model.addAttribute("filter", filter);
             return "main";
         }
 
@@ -41,16 +47,6 @@ public class MainController {
 
         }
 
-        @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-            Iterable<Message> messages;
-            if (filter != null && !filter.isEmpty()) {
-                messages = messasgeRepository.findByTag(filter);
-            } else {
-                messages = messasgeRepository.findAll();
-            }
-            model.put("messages", messages);
-            return "main";
-        }
+
 
     }
