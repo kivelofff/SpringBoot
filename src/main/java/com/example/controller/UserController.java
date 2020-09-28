9ff090ee48b3dff6e30680a4496c966c96b763d6
ladmin <kivelofff@gmail.com>
2020-09-28 18:24:28 +0200

    completed user roles

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,15 +4,19 @@ import com.example.domain.Role;
 import com.example.domain.User;
 import com.example.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -36,7 +40,13 @@ public class UserController {
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user) {
         user.setUsername(username);
-        Arrays.stream()
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
         userRepository.save(user);
         return "redirect:/user";
     }
